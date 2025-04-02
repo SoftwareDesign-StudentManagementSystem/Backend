625316c2d57b39268a079656f794ef3753f12f58
koothemaverick <121041290+koothemaverick@users.noreply.github.com>
2025-04-02 20:54:41 +0900

    Add files via upload

---
+++ src/test/java/com/softwaredesign/SoftwaredesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaredesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwaredesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
