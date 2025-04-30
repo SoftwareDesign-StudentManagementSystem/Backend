86b790b10e8e26715ae7217caff0396e6b405b83
GGingGGang <77926105+GGingGGang@users.noreply.github.com>
2025-04-30 16:11:38 +0900

    Update FeedbackCategory.java
    한글화

--- src/main/java/com/iEdu/domain/studentRecord/feedback/entity/FeedbackCategory.java
+++ src/main/java/com/iEdu/domain/studentRecord/feedback/entity/FeedbackCategory.java
@@ -1,8 +1,8 @@
 package com.iEdu.domain.studentRecord.feedback.entity;
 
 public enum FeedbackCategory {
-    GRADE,      // 성적
-    ATTITUDE,   // 태도
-    ATTENDANCE, // 출결
-    BEHAVIOR    // 행동
+    성적,
+    태도,
+    출결,
+    행동
 }
