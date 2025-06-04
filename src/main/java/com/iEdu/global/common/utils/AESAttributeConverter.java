be61cc9ff06bf3245e4977dda0447edd2f7f699c
koothemaverick <junhui9789@gmail.com>
2025-06-04 16:38:32 +0900

    AESUtil 수정완료.

--- src/main/java/com/iEdu/global/common/utils/AESAttributeConverter.java
+++ src/main/java/com/iEdu/global/common/utils/AESAttributeConverter.java
@@ -9,12 +9,12 @@ public class AESAttributeConverter implements AttributeConverter<String, String>
     @Override
     public String convertToDatabaseColumn(String attribute) {
         if (attribute == null) return null;
-        return AESUtil.encrypt(attribute);
+        return AESUtil.encrypt(attribute); // CBC 방식으로 저장
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
         if (dbData == null) return null;
-        return AESUtil.decrypt(dbData);
+        return AESUtil.safeDecrypt(dbData); // CBC → 실패 시 ECB 시도
     }
 }
